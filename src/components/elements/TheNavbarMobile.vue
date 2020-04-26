6b208f941b7c9fa8d599b96ff7a9c9af89ff3c76
anphungithcmus <anphung1681997@gmail.com>
2020-04-26 09:17:22 +0700

    add

---
+++ src/components/elements/TheNavbarMobile.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="the-navbar-mobile">
+    <div class="brand">
+      <span class="logo">
+        <img
+          src="https://secureservercdn.net/198.71.233.68/3xf.d8b.myftpupload.com/wp-content/uploads/2020/03/logo_qtgroup_003.png?time=1586443498"
+          alt=""
+        />
+      </span>
+    </div>
+    <div class="nav-bar-mobile">
+      <font-awesome-icon :icon="['fa', 'bars']" @click="showSideBar" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheNavbarMobile",
+  methods: {
+    showSideBar: function() {
+      this.$emit("showSideBar");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.the-navbar-mobile {
+  width: 100%;
+  height: 48px;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: center;
+  background: #fff;
+  .brand {
+    width: 95%;
+    float: left;
+    .logo {
+      margin-left: 20px;
+      margin-top: 20px;
+      img {
+        width: 90px;
+      }
+    }
+  }
+  .nav-bar-mobile {
+    float: right;
+    width: 5%;
+    margin-top: 5px;
+    font-size: 20px;
+  }
+}
+</style>
---
+++ src/components/elements/TheSidebarMobile.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="the-sidebar">
+    <div class="side-bar">
+      <ul>
+        <router-link class="side-item" tag="li" to="/">Home</router-link>
+        <router-link class="side-item" tag="li" to="/coronavirus-update"
+          >Coronavirus Update</router-link
+        >
+        <router-link class="side-item" tag="li" to="/about">About</router-link>
+        <router-link class="side-item" tag="li" to="/contact"
+          >Contact</router-link
+        >
+      </ul>
+    </div>
+    <div class="contact-icon">
+      <ul>
+        <a target="_blank" href="" class="twitter"
+          ><font-awesome-icon :icon="['fab', 'twitter']"
+        /></a>
+        <a target="_blank" href="" class="facebook"
+          ><font-awesome-icon :icon="['fab', 'facebook']"
+        /></a>
+        <a target="_blank" href="" class="youtube"
+          ><font-awesome-icon :icon="['fab', 'youtube']"
+        /></a>
+        <a target="_blank" href="" class="vimeo"
+          ><font-awesome-icon :icon="['fab', 'vimeo']"
+        /></a>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Thesidebar"
+}
+</script>
+
+<style lang="scss" scoped>
+.the-sidebar {
+  width: 100%;
+  background: rgb(231, 231, 231);
+  .side-bar {
+    height: 900px;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
+    ul {
+      margin-left: -15px;
+      li {
+        margin-top: 10px;
+        list-style: none;
+        align-items: flex-start;
+        font-weight: 800;
+      }
+      .side-item {
+        padding: 10px;
+        border-radius: 5px;
+        &:hover {
+          cursor: pointer;
+          background-color: #1fb5ac;
+        }
+      }
+      .router-link-exact-active {
+        color: #fff;
+        background-color: #16a085;
+      }
+    }
+  }
+  .contact-icon {
+    height: 100px;
+    ul {
+      font-size: 16px;
+      .twitter {
+        margin-right: 20px;
+        color: #38a1f3;
+        &:hover {
+          cursor: pointer;
+          color: rgb(9, 107, 236);
+          background-color: none;
+        }
+      }
+      .facebook {
+        margin-right: 20px;
+        color: #4267b2;
+        &:hover {
+          cursor: pointer;
+          color: rgb(7, 75, 163);
+          background-color: none;
+        }
+      }
+      .youtube {
+        margin-right: 20px;
+        color: #ed3833;
+        &:hover {
+          cursor: pointer;
+          color: rgb(179, 41, 37);
+          background-color: none;
+        }
+      }
+      .vimeo {
+        margin-right: 20px;
+        color: #86c9ef;
+        &:hover {
+          cursor: pointer;
+          color: rgb(6, 65, 143);
+          background-color: none;
+        }
+      }
+    }
+  }
+}
+</style>
