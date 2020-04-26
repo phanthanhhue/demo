18ea16410542b2d201ae4463e80f38d41041f5d2
Phan Thanh Hue <phanthanhhue@outlook.com>
2020-04-26 11:03:23 +0700

    Modified source

--- src/components/generals/PageHolder.vue
+++ src/components/generals/PageHolder.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="page-holder">
     <router-view />
+    <section class="section-coronavirus-updates">
+      <h1>Coronavirus Update</h1>
+      <CoronavirusUpdate />
+    </section>
+    <section class="section-about">
+      <h1>The About</h1>
+      <About />
+    </section>
+    <section class="sectioin-contact">
+      <h1>The Contact</h1>
+      <Contact />
+    </section>
   </div>
 </template>
 
 <script>
-export default {
+import CoronavirusUpdate from '@/components/pages/CoronavirusUpdate'
+import About from '@/components/pages/TheAbout'
+import Contact from '@/components/pages/TheContact'
 
+export default {
+  name: 'PageHolder',
+  components: {
+    CoronavirusUpdate,
+    About,
+    Contact
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.page-holder {
+  section {
+    h1 {
+      text-align: center;
+      text-transform: uppercase;
+      padding: 40px 0;
+    }
+  }
+}
 </style>
