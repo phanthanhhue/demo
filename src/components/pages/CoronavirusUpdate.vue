04588845c1741d9c2fc942ea22723b3714d255c6
Phan Thanh Hue <phanthanhhue@outlook.com>
2020-04-26 16:38:35 +0700

    Fixed width

--- src/components/pages/CoronavirusUpdate.vue
+++ src/components/pages/CoronavirusUpdate.vue
@@ -3,27 +3,27 @@
     <div>
       <iframe seamless="" scrolling="no"
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTw8X4gXprDvqzoWC_wah6kY6h3ZYOli-7n3FDCj_rgtn7NSU6Q9nj2EUz7-O7Cf1FLXBYeZE-CAUWj/pubchart?oid=1547971389&amp;format=interactive"
-      style="opacity: 1; visibility: visible;" width="967" height="490" frameborder="0"></iframe>
+      style="opacity: 1; visibility: visible;" frameborder="0"></iframe>
     </div>
     <div>
       <iframe seamless="" scrolling="no"
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTw8X4gXprDvqzoWC_wah6kY6h3ZYOli-7n3FDCj_rgtn7NSU6Q9nj2EUz7-O7Cf1FLXBYeZE-CAUWj/pubchart?oid=2011365388&amp;format=interactive"
-      style="opacity: 1; visibility: visible;" width="1020" height="573" frameborder="0"></iframe>
+      style="opacity: 1; visibility: visible;" frameborder="0"></iframe>
     </div>
     <div>
       <iframe seamless="" scrolling="no"
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTw8X4gXprDvqzoWC_wah6kY6h3ZYOli-7n3FDCj_rgtn7NSU6Q9nj2EUz7-O7Cf1FLXBYeZE-CAUWj/pubchart?oid=527963811&amp;format=interactive"
-      style="opacity: 1; visibility: visible;" width="875.5" height="440" frameborder="0"></iframe>
+      style="opacity: 1; visibility: visible;" frameborder="0"></iframe>
     </div>
     <div>
       <iframe seamless="" scrolling="no"
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTw8X4gXprDvqzoWC_wah6kY6h3ZYOli-7n3FDCj_rgtn7NSU6Q9nj2EUz7-O7Cf1FLXBYeZE-CAUWj/pubchart?oid=12462742&amp;format=interactive"
-      style="opacity: 1; visibility: visible;" width="1105" height="510.5" frameborder="0"></iframe>
+      style="opacity: 1; visibility: visible;" frameborder="0"></iframe>
     </div>
     <div>
       <iframe seamless="" scrolling="no"
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTw8X4gXprDvqzoWC_wah6kY6h3ZYOli-7n3FDCj_rgtn7NSU6Q9nj2EUz7-O7Cf1FLXBYeZE-CAUWj/pubchart?oid=1801181287&amp;format=interactive"
-      style="opacity: 1; visibility: visible;" width="988" height="371" frameborder="0"></iframe>
+      style="opacity: 1; visibility: visible;" frameborder="0"></iframe>
     </div>
     <div>
       <p>Source data: https://ncov2019.live/data/global</p>
@@ -40,5 +40,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+// @media (max-width: 768) {
+//   .news {
+//     div {
+//       iframe {
+//         width: 500px;
+//         height: 200px;
+//       }
+//     }
+//   }
+// }
+.news {
+  div {
+    margin-top: 20px;;
+    iframe {
+      width: 100%;
+      height: 500px;
+    }
+  }
+}
 </style>
