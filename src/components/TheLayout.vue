5801ffd40389f693ac110ed491cc59586e1d3cd3
anphungithcmus <anphung1681997@gmail.com>
2020-04-26 07:09:09 +0700

    Update menutop

--- src/components/TheLayout.vue
+++ src/components/TheLayout.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="the-layout">
-    <div class="left-layout">
-    <TheNavbar />
+    <div class="navbar-desktop-layout">
+      <TheNavbar />
     </div>
-    <div class="right-layout">
+    <div class="navbar-mobile-layout">
+      <TheNavbarMobile v-on:showSideBar="showSideBar"/>
+    </div>
+    <div class="content-layout">
+      <TheSidebarMobile v-if="isShowSideBar"/>
       <PageHolder />
       <TheFooter />
     </div>
@@ -12,15 +16,29 @@
 
 <script>
 import TheNavbar from '@/components/elements/TheNavbar'
+import TheNavbarMobile from '@/components/elements/TheNavbarMobile'
+import TheSidebarMobile from '@/components/elements/TheSidebarMobile'
 import PageHolder from '@/components/generals/PageHolder'
 import TheFooter from '@/components/elements/TheFooter'
 
 export default {
   name: 'TheLayout',
+  data () {
+    return {
+      isShowSideBar: false
+    }
+  },
   components: {
     TheNavbar,
+    TheNavbarMobile,
+    TheSidebarMobile,
     PageHolder,
     TheFooter
+  },
+  methods: {
+    showSideBar: function () {
+      this.isShowSideBar =! this.isShowSideBar
+    }
   }
 }
 </script>
@@ -30,7 +48,7 @@ body {
   margin: 0;
   padding: 0;
 }
-.left-layout {
+.navbar-desktop-layout { //Left sidebar for desktop
   border-right: 1px solid rgb(231, 231, 231);
   width: 270px;
   position: fixed;
@@ -39,16 +57,35 @@ body {
   z-index: 9999;
   display:inline-block;
 }
-.right-layout {
-  width: calc(100%-270px);
+.content-layout {
+  width: 100%;
   display: block;
   float: right;
   top: 0;
   left: 0;
 }
+/* Extra large devices (large laptops and desktops, 1000px and up) */
 @media only screen and (min-width: 1000px) {
-  .right-layout {
-    width: calc(100% - 262px);
+  .content-layout {
+    width: calc(100% - 270px);
+  }
+  .navbar-mobile-layout {
+    display:none;
+  }
+}
+@media only screen and (max-width: 1000px) {
+  .navbar-desktop-layout {
+    display:none;
+  }
+  .navbar-mobile-layout {
+    width:100%;
+    position: fixed;
+    top: 0;
+    height: 48px;
+    background: #fff;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
   }
 }
 </style>
